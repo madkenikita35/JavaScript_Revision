2b47935ff76857b74d81673bab7e99f5b8285005
Nikita Satish Madke <madkenikita35@gmail.com>
2026-03-06 23:02:18 +0530

    Add Code

--- Array/SearchingFIinding.js
+++ Array/SearchingFIinding.js
@@ -9,8 +9,8 @@ const index = fruits.findIndex(f => f === 'banana'); // 1
 // includes(): Returns true/false
 const hasApple = fruits.includes('apple'); // true
 
-// // some(): Returns true if AT LEAST ONE item matches
-// const hasLongName = fruits.some(f => f.length > 5); // true (banana)
+// some(): Returns true if AT LEAST ONE item matches
+const hasLongName = fruits.some(f => f.length > 5); // true (banana)
 
 // // every(): Returns true if ALL items match
 // const allLongNames = fruits.every(f => f.length > 2); // true
