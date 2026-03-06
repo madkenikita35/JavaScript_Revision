b1d5300f5bcbff3f8082b7902010f668a8523d36
Nikita Satish Madke <madkenikita35@gmail.com>
2026-03-06 23:06:34 +0530

    Add Code

--- Array/Search.js
+++ Array/Search.js
@@ -10,3 +10,5 @@ const canShip = cart.every(product => product.stock > 0); // false
 // Check if AT LEAST ONE item is out of stock
 const hasAlert = cart.some(product => product.stock === 0); // true
 
+// Find the first out-of-stock item
+const brokenItem = cart.find(product => product.stock === 0); // { item: 'Milk', ... }
