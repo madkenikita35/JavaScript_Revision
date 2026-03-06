d53e6dc39ee6f0e3d0b8880f576c5ee1ae1c4f5f
Nikita Satish Madke <madkenikita35@gmail.com>
2026-03-06 23:01:03 +0530

    Add Code

---
+++ Array/SearchingFIinding.js
@@ -0,0 +1,16 @@
+const fruits = ['apple', 'banana', 'mango'];
+
+// find(): Returns the first element that matches
+const found = fruits.find(f => f.startsWith('m')); // 'mango'
+
+// // findIndex(): Returns the index of that element
+// const index = fruits.findIndex(f => f === 'banana'); // 1
+
+// // includes(): Returns true/false
+// const hasApple = fruits.includes('apple'); // true
+
+// // some(): Returns true if AT LEAST ONE item matches
+// const hasLongName = fruits.some(f => f.length > 5); // true (banana)
+
+// // every(): Returns true if ALL items match
+// const allLongNames = fruits.every(f => f.length > 2); // true
