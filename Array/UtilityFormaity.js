b1eea3502124fba5ff92f80d1361f625a6bc65d5
Nikita Satish Madke <madkenikita35@gmail.com>
2026-03-06 22:58:48 +0530

    Add Code

---
+++ Array/UtilityFormaity.js
@@ -0,0 +1,11 @@
+const fruits = ['apple', 'banana', 'mango'];
+// join(): Turns array into a string
+const tags = ['js', 'react', 'node'];
+console.log(tags.join(' #')); // "js #react #node"
+
+// // flat(): Flattens nested arrays
+// const messy = [1, [2, [3]]];
+// console.log(messy.flat(2)); // [1, 2, 3]
+
+// // slice(): Copies a portion of an array (non-mutating)
+// const part = fruits.slice(0, 2); // ['apple', 'banana']
